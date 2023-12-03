13c708381cb455cbac3987ed666bad41f56aba11
ghostdsb <dibyanshu.bhoi@gmail.com>
2023-12-03 10:43:12 +0530

    feat: wip: template for new day problems

---
+++ aoc2023/_templater/src/bin/part2.rs
@@ -0,0 +1,8 @@
+use DAYVAR::part2::process;
+
+fn main() {
+    let input = include_str!("../../input/final/input-2.txt");
+    let output = process(input);
+    // println!("{}", output);
+    dbg!(output);
+}
---
+++ aoc2023/_templater/src/part1.rs
@@ -0,0 +1,15 @@
+pub fn process(input: &str) -> String {
+    input.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("../input/test/input-1.txt");
+        let output = process(input);
+        assert_eq!(output, "");
+    }
+}
