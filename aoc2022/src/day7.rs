c4a7c4f61719b8fe72eb48905891479f5b5e6126
Dibyanshu <dibyanshu.bhoi@gmail.com>
2024-12-02 10:50:28 +0530

    2022 misc

--- aoc2022/src/day7.rs
+++ aoc2022/src/day7.rs
@@ -1,28 +1,8 @@
 #[allow(unused)]
 pub mod sol {
     #![feature(hash_set_entry)]
-    use std::{fs, vec};
-    use std::collections::HashSet;
-
-    // #[derive(Debug, Eq, Hash, Clone)]
-    // struct Directory{
-    //     name: String,
-    //     dirs: Vec<Directory>,
-    //     files: Vec<String>
-    // }
-
-    // impl PartialEq for Directory{
-    //     fn eq(&self, other: &Directory) -> bool{
-    //         self.name == other.name
-    //     }
-    // }
-
-    // #[derive(Debug)]
-    // struct File{
-    //     name: String,
-    //     size: u64
-    // }
-
+    use std::{fs, collections::BTreeMap};
+    
     pub fn aoc(day: u8, part: u8) -> String {
         let input_path = format!("./input/day{}.txt", day);
         match (fs::read_to_string(&input_path), part) {
@@ -35,12 +15,97 @@ pub mod sol {
         }
     }
 
-    fn part1(content: String) -> String {
+    fn part1(input: String) -> String {
         let mut ans = 0;
+        let mut stack: Vec<&str> = vec![];
+        let mut files: Vec<String> = vec![];
+        let mut hm: BTreeMap<String, u64> = BTreeMap::new();
+        let mut tree: BTreeMap<String, Vec<String>> = BTreeMap::new();
+        input.lines().for_each(|line| {
+            if line.contains("$ cd") {
+                let x = line.split_whitespace().collect::<Vec<&str>>();
+                let dir = x.last().unwrap();
+                if dir == &".." {
+                    stack.pop();
+                } else if dir != &"/" {
+                    stack.push(dir);
+                }
+            } else if !line.contains("$ ls") {
+                let mut location = stack.iter().fold(String::new(), |acc, l| acc + "/" + l);
+                let x = line.split_whitespace().collect::<Vec<&str>>();
+                let file = "/".to_owned() + x[1];
+                let name = location.clone() + &file;
+                if x[0] != "dir"{
+                    hm.insert(name.clone(), x[0].parse::<u64>().unwrap());
+                }
+                tree.entry(location.to_owned()).and_modify(|c| {
+                    c.push(name.clone())
+                }).or_insert(vec![name]);
+            }
+        });
+        tree.keys().into_iter().for_each(|d|{
+            let size = get_size(&tree,&hm, d.to_string());
+            if size <= 100000{
+                ans += size;
+            }
+        } );
         ans.to_string()
     }
 
-    fn part2(content: String) -> String {
-        unimplemented!()
+    fn get_size(tree: &BTreeMap<String, Vec<String>>, file_size: &BTreeMap<String, u64>, file: String) -> u64{
+        let mut ans = 0;
+        let mut stack: Vec<String> = vec![];
+        stack.push(file);
+        while !stack.is_empty(){
+            // println!("{}",stack.len());
+            let top = stack.pop().unwrap();
+            if file_size.contains_key(&top){
+                ans += file_size.get(&top).unwrap();
+            }else if tree.contains_key(&top){
+                tree.get(&top).unwrap().iter().for_each(|c| stack.push(c.to_string()));
+            }
+        }
+        ans
+    }
+
+    fn part2(input: String) -> String {
+        let mut stack: Vec<&str> = vec![];
+        let mut files: Vec<String> = vec![];
+        let mut hm: BTreeMap<String, u64> = BTreeMap::new();
+        let mut tree: BTreeMap<String, Vec<String>> = BTreeMap::new();
+        input.lines().for_each(|line| {
+            if line.contains("$ cd") {
+                let x = line.split_whitespace().collect::<Vec<&str>>();
+                let dir = x.last().unwrap();
+                if dir == &".." {
+                    stack.pop();
+                } else if dir != &"/" {
+                    stack.push(dir);
+                }
+            } else if !line.contains("$ ls") {
+                let mut location = stack.iter().fold(String::new(), |acc, l| acc + "/" + l);
+                let x = line.split_whitespace().collect::<Vec<&str>>();
+                let file = "/".to_owned() + x[1];
+                let name = location.clone() + &file;
+                if x[0] != "dir"{
+                    hm.insert(name.clone(), x[0].parse::<u64>().unwrap());
+                }
+                tree.entry(location.to_owned()).and_modify(|c| {
+                    c.push(name.clone())
+                }).or_insert(vec![name]);
+            }
+        });
+        let total_occupied = get_size(&tree, &hm, "".to_string());
+        dbg!(total_occupied);
+        let required = 30000000 - (70000000 - total_occupied);
+        let ans = tree.keys().into_iter().fold(u64::MAX, |ans,  d|{
+            let s = get_size(&tree,&hm, d.to_string());
+            if s < required{
+                ans
+            }else{
+                ans.min(s)
+            }
+        });
+        ans.to_string()
     }
 }
--- aoc2022/src/main.rs
+++ aoc2022/src/main.rs
@@ -12,40 +12,40 @@ mod day7;
 fn main() {
     println!("Hello, AOC2022!");
 
-    let day1_1 = day1::sol::aoc(1, 1);
-    let day1_2 = day1::sol::aoc(1, 2);
-    println!("day1_1: {}", day1_1);
-    println!("day1_2: {}", day1_2);
+    // let day1_1 = day1::sol::aoc(1, 1);
+    // let day1_2 = day1::sol::aoc(1, 2);
+    // println!("day1_1: {}", day1_1);
+    // println!("day1_2: {}", day1_2);
 
-    let day2_1 = day2::sol::aoc(2, 1);
-    let day2_2 = day2::sol::aoc(2, 2);
-    println!("day2_1: {}", day2_1);
-    println!("day2_2: {}", day2_2);
+    // let day2_1 = day2::sol::aoc(2, 1);
+    // let day2_2 = day2::sol::aoc(2, 2);
+    // println!("day2_1: {}", day2_1);
+    // println!("day2_2: {}", day2_2);
 
-    let day3_1 = day3::sol::aoc(3, 1);
-    let day3_2 = day3::sol::aoc(3, 2);
-    println!("day3_1: {}", day3_1);
-    println!("day3_2: {}", day3_2);
+    // let day3_1 = day3::sol::aoc(3, 1);
+    // let day3_2 = day3::sol::aoc(3, 2);
+    // println!("day3_1: {}", day3_1);
+    // println!("day3_2: {}", day3_2);
 
-    let day4_1 = day4::sol::aoc(4, 1);
-    let day4_2 = day4::sol::aoc(4, 2);
-    println!("day4_1: {}", day4_1);
-    println!("day4_2: {}", day4_2);
+    // let day4_1 = day4::sol::aoc(4, 1);
+    // let day4_2 = day4::sol::aoc(4, 2);
+    // println!("day4_1: {}", day4_1);
+    // println!("day4_2: {}", day4_2);
 
-    let day5_1 = day5::sol::aoc(5, 1);
-    let day5_2 = day5::sol::aoc(5, 2);
-    println!("day5_1: {}", day5_1);
-    println!("day5_2: {}", day5_2);
+    // let day5_1 = day5::sol::aoc(5, 1);
+    // let day5_2 = day5::sol::aoc(5, 2);
+    // println!("day5_1: {}", day5_1);
+    // println!("day5_2: {}", day5_2);
 
-    let day6_1 = day6::sol::aoc(6, 1);
-    let day6_2 = day6::sol::aoc(6, 2);
-    println!("day6_1: {}", day6_1);
-    println!("day6_2: {}", day6_2);
-    
-    let day7_1 = day7::sol::aoc(0, 1);
-    // let day7_2 = day7::sol::aoc(0, 2);
+    // let day6_1 = day6::sol::aoc(6, 1);
+    // let day6_2 = day6::sol::aoc(6, 2);
+    // println!("day6_1: {}", day6_1);
+    // println!("day6_2: {}", day6_2);
+
+    let day7_1 = day7::sol::aoc(7, 1);
+    let day7_2 = day7::sol::aoc(7, 2);
     println!("day7_1: {}", day7_1);
-    // println!("day7_2: {}", day7_2);
+    println!("day7_2: {}", day7_2);
 
     // runner::runner::aoc!();
 
